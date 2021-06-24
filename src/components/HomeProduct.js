3fba5b9aef9ebd5586a5d0273c3eb69e50af2788
Erdem <erdem@studiomawa.com>
2021-06-24 05:27:05 +0300

    Minor updates for home page

--- src/components/HomeProduct.js
+++ src/components/HomeProduct.js
@@ -21,40 +21,12 @@ class HomeProduct extends React.Component {
           <h3 className="product-name">
             <a href="/product">Ride it!</a>
           </h3>
-          <h4 className="product-price">CAD 1,899</h4>
+
           <h5 className="product-old-price">CAD 2,299</h5>
+          <h4 className="product-price">CAD 1,899</h4>
           <a href="/product" className="explore-button">
-            Explore our e-bikes
+            Explore more
           </a>
-          {/* <div className="product-description">
-            <p>
-              Take on long distance and uphill adventures like a pro. With our
-              innovative Torque Sensor System, you can make any challenging
-              environment seems effortless.
-            </p>
-          </div>
-          <div className="product-rotates">
-            <div className="product-rotate rx">
-              <span>X</span>
-            </div>
-            <div className="product-rotate ry">
-              <span>Y</span>
-            </div>
-          </div>
-          <div className="product-actions">
-            <div className="product-action add-to-cart">
-              <div className="icon">
-                <span></span>
-              </div>
-              <a href="/">Add to cart</a>
-            </div>
-            <div className="product-action check-out-now">
-              <div className="icon">
-                <span></span>
-              </div>
-              <a href="/checkout">Check out now</a>
-            </div>
-          </div> */}
         </div>
       </div>
     );
