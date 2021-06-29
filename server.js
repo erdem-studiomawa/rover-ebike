6714d34c944881407a02830ffbf896591031c448
Erdem <erdem@studiomawa.com>
2021-06-30 00:54:56 +0300

    Checkout Page - Stripe payment api

---
+++ server.js
@@ -0,0 +1,57 @@
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+const express = require("express");
+const app = express();
+app.use(express.static("."));
+
+const YOUR_DOMAIN = "http://localhost:3000/checkout";
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "Stubborn Attachments",
+            images: ["https://i.imgur.com/EHyR2nP.png"],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.json({ id: session.id });
+});
+
+app.listen(4242, () => console.log("Running on port 4242"));
--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
+import { loadStripe } from "@stripe/stripe-js";
 
 import "./index.css";
 
@@ -6,20 +7,60 @@ import { CartContext } from "../../contexts/CartContext";
 import { formatNumber } from "../../helpers/utils";
 
 import setTitle from "../../tools.js";
-import headerImage from "../../assets/images/checkout-page-header.png";
 
 import { DeleteOutline, Search } from "@material-ui/icons";
+import mastercardIcon from "../../assets/icons/payment-methods/mastercard.png";
+import visaIcon from "../../assets/icons/payment-methods/visa.png";
+import maestroIcon from "../../assets/icons/payment-methods/maestro.png";
+import americanexpressIcon from "../../assets/icons/payment-methods/americanexpress.png";
+import paypalIcon from "../../assets/icons/payment-methods/paypal.png";
+
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const CheckoutPage = () => {
+  useEffect(() => {
+    setTitle("Checkout");
+  }, []);
+
   const { total, cartItems, increase, decrease, removeProduct } =
     useContext(CartContext);
+  const [paymentMethod, setPaymentMethod] = useState();
+  const [processing, setProcessing] = useState(false);
+  const submitButtonRef = useRef();
+
+  const handleClick = async (event) => {
+    setProcessing(true);
+    if (paymentMethod === "stripe") {
+      const stripe = await stripePromise;
+      const response = await fetch(
+        "http://localhost:4242/create-checkout-session",
+        {
+          method: "POST",
+        }
+      );
+      const session = await response.json();
+      // When the customer clicks on the button, redirect them to Checkout.
+      const result = await stripe.redirectToCheckout({
+        sessionId: session.id,
+      });
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer
+        // using `result.error.message`.
+      }
+    } else {
+      const response = await fetch("https://www.paypal.com/", {
+        method: "POST",
+      });
+    }
+  };
 
   return (
     <div className="checkout-page">
       <div className="checkout-wrapper">
         <div className="checkout-form">
-          <h4>Personal Information</h4>
-          <form action="/" method={"POST"}>
+          <h4>Shipping Details</h4>
+          <form action="#" method={"POST"}>
             <div className="form-group">
               <label>*Email Address</label>
               <input type={"text"}></input>
@@ -34,16 +75,9 @@ const CheckoutPage = () => {
                 <input type={"text"}></input>
               </div>
             </div>
-            <div className="form-group">
-              <label>*Message</label>
-              <textarea></textarea>
-            </div>
+
             <div className="form-group">
               <label>*Street and house number</label>
-              <input type={"text"}></input>
-            </div>
-            <div className="form-group">
-              <label>*Zip/Postal Code</label>
               <input type={"text"}></input>
             </div>
             <div className="form-group row">
@@ -70,7 +104,114 @@ const CheckoutPage = () => {
               </div>
             </div>
             <div className="form-group">
-              <button type={"submit"}>Submit</button>
+              <label>*Zip/Postal Code</label>
+              <input type={"text"}></input>
+            </div>
+
+            <h4>Payment</h4>
+            <div className="form-group">
+              <div className="payment-methods">
+                <div className="payment-method">
+                  <div className="payment-method-select">
+                    <input
+                      type="radio"
+                      name="payment-method"
+                      id="payment-method-cc"
+                      onClick={() => setPaymentMethod("stripe")}
+                    />
+                    <label for="payment-method-cc">Credit/Debit Cards</label>
+                  </div>
+                  <div className="payment-method-logos">
+                    <img src={visaIcon} />
+                    <img src={mastercardIcon} />
+                    <img src={maestroIcon} />
+                    <img src={americanexpressIcon} />
+                  </div>
+                </div>
+                <div className="payment-method">
+                  <div className="payment-method-select">
+                    <input
+                      type="radio"
+                      name="payment-method"
+                      id="payment-method-paypal"
+                      onClick={() => setPaymentMethod("paypal")}
+                    />
+                    <label for="payment-method-paypal">Paypal</label>
+                  </div>
+                  <div className="payment-method-logos">
+                    <img src={paypalIcon} width={50} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <h4>Billing Address</h4>
+            <div className="dark-box">
+              <div className="form-group row">
+                <input
+                  type="radio"
+                  name="shipping-address"
+                  id="shipping-address-1"
+                  className="inherit"
+                />
+                <label for="shipping-address-1">
+                  Same as shipping address.
+                </label>
+              </div>
+              <div className="form-group row">
+                <input
+                  type="radio"
+                  name="shipping-address"
+                  id="shipping-address-2"
+                  className="inherit"
+                />
+                <label for="shipping-address-2">
+                  Different billing address.
+                </label>
+              </div>
+            </div>
+
+            <h4>Review and Place Order</h4>
+            <div className="dark-box">
+              <div className="form-group row">
+                <input
+                  type="radio"
+                  name="review-approve"
+                  id="review-approve"
+                  className="inherit"
+                />
+                <label for="review-approve">
+                  Please review the order details above, and when you're ready,
+                  click Place Order.
+                </label>
+              </div>
+              <div className="form-group row">
+                <input
+                  type="radio"
+                  name="terms-approve"
+                  id="terms-approve"
+                  className="inherit"
+                />
+                <label for="terms-approve">
+                  I agree to the Terms & Conditions,Privacy Policy and Return
+                  Policy
+                </label>
+              </div>
+            </div>
+
+            <div className="form-group buttons">
+              {processing ? (
+                <button type={"button"}>...</button>
+              ) : (
+                <button
+                  type={"button"}
+                  onClick={() => {
+                    handleClick();
+                  }}
+                >
+                  Place Order
+                </button>
+              )}
             </div>
           </form>
         </div>
